fa8f8de20a72747c2b485a3729fe2ad250f93ba4
Koutilya Namdeo <koutilyanamdeo@Mac.lan>
2026-05-14 21:06:03 +0530

    changes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,9 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 // import { PrismaClient } from '../../generated/prisma/client.ts';
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg as { PrismaClient: any };
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaMariaDb({
-
  host: process.env.DATABASE_HOST || "127.0.0.1",
   port: Number(process.env.DATABASE_PORT) || 3306, // Add this line!
   user: process.env.DATABASE_USER || "root",
